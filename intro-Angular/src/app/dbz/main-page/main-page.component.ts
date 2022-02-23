443552aef618c0b505f4a9c0e0b5d0c64b628e2c
jelsync <jelsyn@gmail.com>
2022-02-23 12:20:37 -0600

    ngModel, catpura/escucha eventos

--- intro-Angular/src/app/dbz/main-page/main-page.component.ts
+++ intro-Angular/src/app/dbz/main-page/main-page.component.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
 
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
 
-  agregar( ){
+  nuevo: Personaje = {
+    nombre: 'Goku',
+    poder: 30000
+  }
+
+  agregar() {
     // agregar( event: any ){
     // event.preventDefault();
-    console.log('hEY')
+    console.log(this.nuevo)
+  }
+  cambiarNombre(event: any){
+    console.log(event.target.value)
   }
 
 }
